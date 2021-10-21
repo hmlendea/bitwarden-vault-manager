2bcf8a9a16c61b676675190c5403cbf05c04233e
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-10-21 14:19:33 +0300

    Blank CLI menu

---
+++ Menus/MainMenu.cs
@@ -0,0 +1,18 @@
+using NuciCLI.Menus;
+
+namespace BitwardenVaultManager.Menus
+{
+    /// <summary>
+    /// Main menu.
+    /// </summary>
+    public class MainMenu : Menu
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainMenu"/> class.
+        /// </summary>
+        public MainMenu() : base("Bitwarden Vault Manager")
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Text;
+
+using NuciCLI.Menus;
+
+using BitwardenVaultManager.Menus;
 
 namespace BitwardenVaultManager
 {
     class Program
     {
-        static void Main(string[] args)
+        static string applicationDirectory;
+
+        /// <summary>
+        /// Gets the application directory.
+        /// </summary>
+        /// <value>The application directory.</value>
+        public static string ApplicationDirectory
         {
-            Console.WriteLine("Hello World!");
+            get
+            {
+                if (string.IsNullOrEmpty(applicationDirectory))
+                {
+                    applicationDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                }
+
+                return applicationDirectory;
+            }
+        }
+
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            MenuManager.Instance.AreStatisticsEnabled = true;
+            MenuManager.Instance.Start<MainMenu>();
         }
     }
 }
